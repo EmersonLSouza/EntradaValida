688f5cc35a9e661da4cb905f51a2697cf83015fe
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2024-05-21 22:34:49 -0300

    Exercício Resolvido

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿﻿Console.WriteLine("--- Entrada Válida ---\n");
+
+int numero = -1;
+
+while (numero < 0 || numero > 9)
+{
+    Console.Write("Digite um número entre 1 e 9 (ou 0 para cancelar)...: ");
+    numero = Convert.ToInt32(Console.ReadLine());
+}
+
+if (numero == 0)
+{
+    Console.WriteLine("\nOperação cancelada.");
+}
+else
+{
+    Console.WriteLine($"\nNúmero selecionado = {numero}");
+}
+
+Console.WriteLine("\nVolte sempre!");
